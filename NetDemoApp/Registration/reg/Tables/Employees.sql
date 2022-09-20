ff5f53eb30a585b14e86a0b77127210c2749e33f
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2022-09-20 10:33:49 +0200

    Add initial tables

---
+++ NetDemoApp/Registration/reg/Tables/Employees.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [reg].[Employees]
+(
+    [Id]            smallint NOT NULL IDENTITY(1,1),
+    [FirstName]     nvarchar(100) NOT NULL,
+    [LastName]      nvarchar(100) NOT NULL,
+    [BirthDate]     date NOT NULL,
+    [OfficeId]      smallint NOT NULL,
+    CONSTRAINT PK_Employees PRIMARY KEY CLUSTERED ([Id]),
+    CONSTRAINT FK_EmployeeOffice FOREIGN KEY (OfficeId) REFERENCES [reg].[Offices]([Id]),
+);
---
+++ NetDemoApp/Registration/reg/Tables/Offices.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [reg].[Offices]
+(
+    [Id]            smallint NOT NULL IDENTITY(1,1) PRIMARY KEY CLUSTERED,
+    [Location]      nvarchar(50) NOT NULL,
+    [MaxOccupancy]  smallint
+);
