968f481f6567e009fd593e896bde5426762c1cba
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2022-09-20 10:33:50 +0200

    Disable implicit usings for DemoApi

--- NetDemoApp/DemoApi/Program.cs
+++ NetDemoApp/DemoApi/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+
 namespace DemoApi
 {
     public class Program
--- NetDemoApp/DemoApi/WeatherForecast.cs
+++ NetDemoApp/DemoApi/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DemoApi
 {
     public class WeatherForecast
