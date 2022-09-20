a036a958c854f384787cf0ce360bf9eed1c854e3
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2022-09-20 10:33:50 +0200

    Add test-project

---
+++ NetDemoApp/DemoApi.Tests/EmployeeRepositoryTests.cs
@@ -0,0 +1,15 @@
+namespace DemoApi.Tests;
+
+public class EmployeeRepositoryTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
