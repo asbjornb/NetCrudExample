b6fa4f5fde71affbceb50b0d05847705dc88d235
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2022-09-20 10:33:54 +0200

    Simplify tests a bit

--- NetDemoApp/DemoApi.Tests/DatabaseReliantTests/EmployeeRepositoryTests.cs
+++ NetDemoApp/DemoApi.Tests/DatabaseReliantTests/EmployeeRepositoryTests.cs
@@ -12,6 +12,7 @@ namespace DemoApi.Tests.DatabaseReliantTests;
 public class EmployeeRepositoryTests
 {
     private readonly string employeeTable = typeof(EmployeePoco).GetCustomAttribute<TableNameAttribute>().Value;
+    private const string officeTable = "reg.Offices";
     private EmployeeRepository sut;
     private DatabaseProvider databaseProvider;
 
@@ -22,8 +23,7 @@ public class EmployeeRepositoryTests
         databaseProvider = new DatabaseProvider(SetupDatabaseForTests.ConnectionString);
 
         //Insert office for foreign key
-        using var database = databaseProvider.GetDatabase();
-        await database.ExecuteAsync("INSERT INTO reg.Offices(Location, MaxOccupancy) VALUES(@0, @1);", "TestOffice", 10);
+        await InsertOffice(1, "TestOffice", 2);
     }
 
     [SetUp]
@@ -38,6 +38,7 @@ public class EmployeeRepositoryTests
         //Reset table after each test
         using var database = databaseProvider.GetDatabase();
         await database.ExecuteAsync($"TRUNCATE TABLE {employeeTable};");
+        await database.ExecuteAsync($"DELETE FROM {officeTable} WHERE Id>@0;", 1);
     }
 
     [Test]
@@ -48,7 +49,7 @@ public class EmployeeRepositoryTests
 
         using var database = databaseProvider.GetDatabase();
         var rows = database.Fetch<EmployeePoco>();
-        rows.Should().HaveCount(1);
+        rows.Should().ContainSingle();
         var row = rows.Single();
         row.FirstName.Should().Be(employee.FirstName);
         row.LastName.Should().Be(employee.LastName);
@@ -61,31 +62,28 @@ public class EmployeeRepositoryTests
     public async Task ShouldDeleteEmployee()
     {
         const int id = 1;
-        using var database = databaseProvider.GetDatabase();
-        database.Execute("SET IDENTITY_INSERT reg.Employees ON; " +
-            "INSERT INTO reg.Employees(Id, FirstName, LastName, Birthdate, OfficeId) VALUES(@0, @1, @2, @3, @4); " +
-            "SET IDENTITY_INSERT reg.Employees OFF;", id, "John", "Doe", new DateTime(1990, 1, 1), 1);
+
+        await InsertEmployeeWithId(id, "John", "Doe", new DateTime(1990, 1, 1), 1);
 
         var result = await sut.DeleteEmployeeAsync(id);
 
+        using var database = databaseProvider.GetDatabase();
         var rows = database.Fetch<EmployeePoco>();
         rows.Should().HaveCount(0);
-        result.Should().Be(true);
+        result.Should().BeTrue();
     }
 
     [Test]
     public async Task ShouldNotDeleteOtherEmployees()
     {
-        using var database = databaseProvider.GetDatabase();
-        database.Execute("SET IDENTITY_INSERT reg.Employees ON; " +
-            "INSERT INTO reg.Employees(Id, FirstName, LastName, Birthdate, OfficeId) VALUES(@0, @1, @2, @3, @4); " +
-            "SET IDENTITY_INSERT reg.Employees OFF;", 1, "John", "Doe", new DateTime(1990, 1, 1), 1);
+        await InsertEmployeeWithId(1, "John", "Doe", new DateTime(1990, 1, 1), 1);
 
         var result = await sut.DeleteEmployeeAsync(5);
 
+        using var database = databaseProvider.GetDatabase();
         var rows = database.Fetch<EmployeePoco>();
-        rows.Should().HaveCount(1);
-        result.Should().Be(false);
+        rows.Should().ContainSingle();
+        result.Should().BeFalse();
     }
 
     [Test]
@@ -96,11 +94,8 @@ public class EmployeeRepositoryTests
         const string lastName = "Doe";
         var birthDate = new DateTime(1990, 1, 1);
         const int officeId = 1;
-        
-        using var database = databaseProvider.GetDatabase();
-        database.Execute("SET IDENTITY_INSERT reg.Employees ON; " +
-            "INSERT INTO reg.Employees(Id, FirstName, LastName, Birthdate, OfficeId) VALUES(@0, @1, @2, @3, @4); " +
-            "SET IDENTITY_INSERT reg.Employees OFF;", id, firstName, lastName, birthDate, officeId);
+
+        await InsertEmployeeWithId(id, firstName, lastName, birthDate, officeId);
 
         var result = await sut.GetEmployeeAsync(id);
 
@@ -114,13 +109,24 @@ public class EmployeeRepositoryTests
     [Test]
     public async Task ShouldGetNullIfEmployeeNotExists()
     {
-        using var database = databaseProvider.GetDatabase();
-        database.Execute("SET IDENTITY_INSERT reg.Employees ON; " +
-            "INSERT INTO reg.Employees(Id, FirstName, LastName, Birthdate, OfficeId) VALUES(@0, @1, @2, @3, @4); " +
-            "SET IDENTITY_INSERT reg.Employees OFF;", 1, "John", "Doe", new DateTime(1990, 1, 1), 1);
+        await InsertEmployeeWithId(1, "John", "Doe", new DateTime(1990, 1, 1), 1);
 
         var result = await sut.GetEmployeeAsync(5);
 
         result.Should().BeNull();
+    }
+
+    private async Task InsertEmployeeWithId(int id, string firstName, string lastName, DateTime birthDate, int officeId)
+    {
+        using var database = databaseProvider.GetDatabase();
+        await database.ExecuteAsync($"SET IDENTITY_INSERT {employeeTable} ON; " +
+                    $"INSERT INTO {employeeTable}(Id, FirstName, LastName, Birthdate, OfficeId) VALUES(@0, @1, @2, @3, @4); " +
+                    $"SET IDENTITY_INSERT {employeeTable} OFF;", id, firstName, lastName, birthDate, officeId);
+    }
+
+    private async Task InsertOffice(int id, string location, int maxOccupancy)
+    {
+        using var database = databaseProvider.GetDatabase();
+        await database.ExecuteAsync($"SET IDENTITY_INSERT {officeTable} ON; INSERT INTO {officeTable}(Id, Location, MaxOccupancy) VALUES(@0, @1, @2); SET IDENTITY_INSERT {officeTable} OFF; ", id, location, maxOccupancy);
     }
 }
