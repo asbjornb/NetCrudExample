918f9eb84710210993986806720755ebda7147a2
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2022-09-20 10:33:51 +0200

    Add not null and fix spelling in tables

--- NetDemoApp/Registration/reg/Tables/Offices.sql
+++ NetDemoApp/Registration/reg/Tables/Offices.sql
@@ -2,5 +2,5 @@
 (
     [Id]            smallint NOT NULL IDENTITY(1,1) PRIMARY KEY CLUSTERED,
     [Location]      nvarchar(50) NOT NULL,
-    [MaxOccupancy]  smallint
+    [MaxOccupancy]  smallint NOT NULL,
 );
