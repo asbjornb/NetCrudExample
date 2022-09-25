e5686710ba4139d902d83a2e48e75f18c63bcd9f
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2022-09-25 21:43:55 +0200

    Add stored procedures to handle MaxOccupancy

---
+++ NetDemoApp/Registration/reg/Stored Procedures/InsertEmployee.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE reg.InsertEmployee
+    @FirstName nvarchar(100), @LastName nvarchar(100), @Birthdate date, @OfficeId smallint
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    IF @FirstName IS NULL OR @LastName IS NULL OR @Birthdate IS NULL OR @OfficeId IS NULL
+    BEGIN
+        RAISERROR('All parameters are required to be not null', 16, 1);
+    END
+    
+    SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+    
+    IF (SELECT COUNT(*) FROM reg.Employees e WHERE e.OfficeId = @OfficeId) < (SELECT o.MaxOccupancy FROM reg.Offices o WHERE o.Id = @OfficeId)
+    BEGIN
+        INSERT INTO reg.Employees (FirstName, LastName, Birthdate, OfficeId)
+        VALUES (@FirstName, @LastName, @Birthdate, @OfficeId)
+        
+        SELECT SCOPE_IDENTITY() AS Id
+    END
+    ELSE
+    BEGIN
+        RAISERROR('Max occupancy already reached for office with id %i. Failed to insert employee', 11, -1, @OfficeId)
+	END
+    
+    RETURN 0
+END
---
+++ NetDemoApp/Registration/reg/Stored Procedures/UpdateEmployee.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE reg.UpdateEmployee
+    @Id int, @FirstName nvarchar(100), @LastName nvarchar(100), @Birthdate date, @OfficeId smallint
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    IF @Id IS NULL OR @FirstName IS NULL OR @LastName IS NULL OR @Birthdate IS NULL OR @OfficeId IS NULL
+    BEGIN
+        RAISERROR('All parameters are required to be not null', 16, 1);
+    END
+    
+    SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+    
+    IF (SELECT COUNT(*) FROM reg.Employees e WHERE e.OfficeId = @OfficeId AND e.Id != @Id) < (SELECT o.MaxOccupancy FROM reg.Offices o WHERE o.Id = @OfficeId)
+    BEGIN
+        UPDATE reg.Employees
+        SET FirstName = @FirstName, LastName = @LastName, Birthdate = @Birthdate, OfficeId = @OfficeId
+        WHERE Id = @Id
+        
+        SELECT @@ROWCOUNT AS RowsAffected
+    END
+    ELSE
+    BEGIN
+        RAISERROR('Max occupancy already reached for office with id %i. Failed to insert employee', 11, -1, @OfficeId)
+	END
+    
+    RETURN 0
+END
